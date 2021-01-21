d78554e0845c3c2f01e692c021ae32a3eb76c7ab
Joao Tinti <joaotinti75@gmail.com>
2021-01-21 09:09:04 -0300

    diretorios imagens e sons adicionados

---
+++ dino.py
@@ -0,0 +1,58 @@
+import os
+import pygame
+from pygame.locals import *
+from sys import exit
+
+diretorio_principal = os.path.dirname(__file__)
+diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
+
+LARGURA = 640
+ALTURA = 480
+
+PRETO = (0,0,0)
+BRANCO = (255,255,255)
+
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+
+pygame.display.set_caption('Dino')
+
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpritesheet.png')).convert_alpha()
+
+class Dino(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagens_dinossauro = []
+        for i in range(3):
+            img = sprite_sheet.subsurface((i*32, 0), (32,32))
+            img = pygame.transform.scale(img, (96, 96))
+            self.imagens_dinossauro.append(img)
+
+        self.index_lista = 0
+        self.image = self.imagens_dinossauro[self.index_lista]
+        self.rect = self.image.get_rect()
+        self.rect.center = (100,100)
+
+    def update(self):
+        if self.index_lista > 2:
+            self.index_lista = 0
+
+        self.index_lista += 0.25
+        self.image = self.imagens_dinossauro[int(self.index_lista)]
+    
+todas_as_sprites = pygame.sprite.Group()
+dino = Dino()
+todas_as_sprites.add(dino)
+
+relogio = pygame.time.Clock()
+while True:
+    relogio.tick(30)
+    tela.fill(BRANCO)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+
+    pygame.display.flip()
