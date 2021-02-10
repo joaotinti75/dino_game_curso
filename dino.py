51afdd656b798198df5b5a7df17e79cb7e78b781
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2021-02-10 08:11:19 -0300

    aula 3

--- dino.py
+++ dino.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from sys import exit
 import os
+from random import randrange
 
 diretorio_principal = os.path.dirname(__file__)
 diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
@@ -30,18 +31,36 @@ class Dino(pygame.sprite.Sprite):
         self.index_lista = 0
         self.image = self.imagens_dinossauro[self.index_lista]
         self.rect = self.image.get_rect()
-        self.rect.center = (100,100)
+        self.rect.center = (100, ALTURA - 90)
 
     def update(self):
         if self.index_lista > 2:
             self.index_lista = 0
         self.index_lista += 0.25
         self.image = self.imagens_dinossauro[int(self.index_lista)]
-
     
+class Nuvens(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((7*32, 0), (32,32))
+        self.image = pygame.transform.scale(self.image, (32*3, 32*3))
+        self.rect = self.image.get_rect()
+        self.rect.y = randrange(50, 200, 50)
+        self.rect.x = LARGURA - randrange(30, 300, 90)
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+            self.rect.y = randrange(50, 200, 50)
+        self.rect.x -= 10
+
 todas_as_sprites = pygame.sprite.Group()
 dino = Dino()
 todas_as_sprites.add(dino)
+
+for i in range(4):
+    nuvem = Nuvens()
+    todas_as_sprites.add(nuvem)
 
 relogio = pygame.time.Clock()
 while True:
