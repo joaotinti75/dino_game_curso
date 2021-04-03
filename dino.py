4dec9de8b6b372a34984d007d4846dfa8f8a6c1f
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2021-04-03 17:43:40 -0300

    Update dino.py

--- dino.py
+++ dino.py
@@ -24,9 +24,23 @@ sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpriteshee
 
 som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'death_sound.wav'))
 som_colisao.set_volume(1)
+
+som_pontuacao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'score_sound.wav'))
+som_pontuacao.set_volume(1)
+
 colidiu = False
 
 escolha_obstaculo = choice([0, 1])
+
+pontos = 0
+
+velocidade_jogo = 10
+
+def exibe_mensagem(msg, tamanho, cor):
+    fonte = pygame.font.SysFont('comicsansms', tamanho, True, False)
+    mensagem = f'{msg}' 
+    texto_formatado = fonte.render(mensagem, True, cor)
+    return texto_formatado
 
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
@@ -83,7 +97,7 @@ class Nuvens(pygame.sprite.Sprite):
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA
             self.rect.y = randrange(50, 200, 50)
-        self.rect.x -= 10
+        self.rect.x -= velocidade_jogo
 
 class Chao(pygame.sprite.Sprite):
     def __init__(self, pos_x):
@@ -110,12 +124,11 @@ class Cacto(pygame.sprite.Sprite):
         self.rect.center = (LARGURA,  ALTURA - 64)
         self.rect.x = LARGURA
 
-
     def update(self):
         if self.escolha == 0:
             if self.rect.topright[0] < 0:
                 self.rect.x = LARGURA
-            self.rect.x -= 10
+            self.rect.x -= velocidade_jogo
 
 class DinoVoador(pygame.sprite.Sprite):
     def __init__(self):
@@ -138,7 +151,7 @@ class DinoVoador(pygame.sprite.Sprite):
         if self.escolha == 1:
             if self.rect.topright[0] < 0:
                 self.rect.x = LARGURA
-            self.rect.x -= 10
+            self.rect.x -= velocidade_jogo
 
             if self.index_lista > 1:
                 self.index_lista = 0
@@ -193,18 +206,26 @@ while True:
         cacto.escolha = escolha_obstaculo
         dino_voador.escolha = escolha_obstaculo
 
-
     if colisoes and colidiu == False:
         som_colisao.play()
         colidiu = True
 
     if colidiu == True:
+        if pontos % 100 == 0:
+            pontos += 1
         pass
     else:
+        pontos += 1
         todas_as_sprites.update()
+        texto_pontos = exibe_mensagem(pontos, 40, (0,0,0))
 
-    pygame.display.flip()
-    
-    
+    if pontos % 100 == 0:
+        som_pontuacao.play()
+        if velocidade_jogo >= 23:
+            velocidade_jogo += 0
+        else:
+            velocidade_jogo += 1
+        
+    tela.blit(texto_pontos, (520, 30))
 
     pygame.display.flip()
